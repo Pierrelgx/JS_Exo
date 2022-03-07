7c9936963e4caf7e9b281112bff66b21d1c3b570
PierreLgx <97539842+Pierrooow@users.noreply.github.com>
2022-03-07 13:26:35 +0100

    🍀 ça continue d'accord d'accoooord

--- script.js
+++ script.js
@@ -12,8 +12,18 @@ function footerAlert() {
     }
  footerAlert()
 
+  
  // Exercice 2
  // On fait un satané menu HAMBURGEEER (I Would like to buy an hamburgeeer)
+ function hamburgerMenu() {
+	nav = document.getElementById("navbarHeader")
+	hambuger = document.querySelector("button", ".navbar-toggler")
+	hambuger.addEventListener("click", function () {
+		nav.classList.toggle("collapse")
+	})
+}
+
+hamburgerMenu()
 
 
  // Exercie 3
@@ -28,23 +38,18 @@ function footerAlert() {
 }
 
 redText()
-
+ 
 
 // Exercice 4
 // Magie aussi, en green cette fois, mais possible de revenir en arrière, avec un toggle !
-function greenText () {
-editCardButtonTwo = document.querySelectorAll(".btn-outline-secondary")[1]
-textCardTwo = document.querySelectorAll(".card-text")[1]
-editCardButtonTwo.addEventListener("click", function() {
-        textCardTwo.style.color = "green"
 
-        
-})
-
-}
-
-greenText()
 
 
 // Exercice 5
 // Le nucléaire attention c'est dangereux ☢️
+
+var p = document.querySelectorAll('p')
+var rougit = function() {
+    p.classList.add('red')
+}
+p.addEventListener('click', rougit)
